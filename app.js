d27aa2a16926f89993cda3d725c247553bed1630
Mesut <yildirimmesutyigit@gmail.com>
2023-05-19 21:28:11 +0300

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-let sentence =
-  "People should not be afraid of their governments; the government should be afraid of their people.";
+// let sentence =
+//   "People should not be afraid of their governments; the government should be afraid of their people.";
 
-console.log(sentence.split(" ").reverse().join(" "));
+// console.log(sentence.split(" ").reverse().join(" "));
+
+// let sentence = prompt("Give a sentence to reverse").split(" ");
+// function reversedSentence(a) {
+//   let newStr = "";
+//   for (let i = a.length - 1; i >= 0; i--) {
+//     newStr += a[i];
+//   }
+//   return newStr;
+// }
+// console.log(reversedSentence(sentence));
+
+let sentence = prompt("Give a sentence to reverse").split(" ");
+function reversedSentence(a) {
+  let newStr = "";
+  for (let i = a.length - 1; i >= 0; i--) {
+    newStr += a[i];
+  }
+  return newStr;
+}
+console.log(reversedSentence(sentence));
